a17221803c2340f7770bde58e152dc6c9a75c85a
Tarsis Lima <br.tmvdl@gmail.com>
2024-09-24 10:50:14 +0000

    Updating app files

---
+++ projects/twitch-websocket-api/components/header.component.js
@@ -0,0 +1,41 @@
+import { HTML, nFlex, nInput, nButton } from '../../../assets/js/libs/frontend/index.js'
+
+export class HeaderComponent extends HTML {
+  children = {
+    url: new nInput(),
+    connect: new nButton(),
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.append(this.getFlex())
+  }
+
+  getFlex() {
+    const flex = new nFlex()
+    flex.append(this.getUrlInput().setContainerStyle('width', '80%'))
+    flex.append(this.getConnectButton().setContainerStyle('width', '20%'))
+    return flex
+  }
+
+  getUrlInput() {
+    this.children.url.setContainerStyle('padding', '1rem')
+    this.children.url.setStyle('padding', 'calc(1rem / 4)')
+    this.children.url.setStyle('width', '100%')
+    this.children.url.setPlaceholder('url')
+    return this.children.url
+  }
+
+  getConnectButton() {
+    this.children.connect.setContainerStyle('padding', '1rem')
+    this.children.connect.setStyle('padding', 'calc(1rem / 4)')
+    this.children.connect.setStyle('width', '100%')
+    this.children.connect.setText('connect')
+    this.children.connect.addEventListener('click', () => this.onConnectButtonClick())
+    return this.children.connect
+  }
+
+  onConnectButtonClick() {
+    this.dispatch('connect', this.children.url.getValue())
+  }
+}
---
+++ projects/twitch-websocket-api/config.js
@@ -0,0 +1,7 @@
+// 
+
+export const url = ''
+
+export const client_id = ''
+
+export const client_secret = ''
