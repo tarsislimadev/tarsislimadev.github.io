3c96b6bb8145ef6500f40fab058d1d1b1cfeb08f
Tarsis Lima <br.tmvdl@gmail.com>
2024-09-24 10:50:11 +0000

    Updating app files

---
+++ projects/imc/index.js
@@ -0,0 +1,55 @@
+import { HTML } from '../../assets/js/libs/frontend/index.js'
+import { PaddingComponent } from '../../assets/js/components/padding.component.js'
+import { ButtonComponent } from '../../assets/js/components/button.component.js'
+import { InputComponent } from '../../assets/js/components/input.component.js'
+import { TextComponent } from '../../assets/js/components/text.component.js'
+import { getParams } from '../../assets/js/utils/url.js'
+
+export class Page extends PaddingComponent {
+  children = {
+    weight: new InputComponent({ label: 'weight (kg)', value: 80, type: 'number' }),
+    height: new InputComponent({ label: 'height (m)', value: 1.8, type: 'number' }),
+    result: new HTML(),
+  }
+
+  state = { params: getParams({ title: 'IMC' }) }
+
+  onCreate() {
+    super.onCreate()
+    this.append(new TextComponent({ text: this.state.params.title }))
+    this.append(this.getBody())
+  }
+
+  getBody() {
+    const html = new HTML()
+    html.setStyle('max-width', '26rem')
+    html.append(this.getWeightInput())
+    html.append(this.getHeightInput())
+    html.append(new ButtonComponent({ text: 'calc imc', onclick: () => this.onImcButtonClick() }))
+    html.append(this.getResultHTML())
+    return html
+  }
+
+  getWeightInput() {
+    return this.children.weight
+  }
+
+  getHeightInput() {
+    return this.children.height
+  }
+
+  onImcButtonClick() {
+    const weight = this.children.weight.getValue()
+    const height = this.children.height.getValue()
+    const imc = this.calcIMC(weight, height)
+    this.children.result.setText(`IMC: ${imc}`)
+  }
+
+  getResultHTML() {
+    return this.children.result
+  }
+
+  calcIMC(weight = 1, height = 1) {
+    return (weight / (height * height)).toFixed(4)
+  }
+}
---
+++ projects/imc/input.component.js
@@ -0,0 +1,51 @@
+import { HTML, nInput } from '../../assets/js/libs/frontend/index.js'
+import { getParams } from './params.js'
+
+export class InputComponent extends HTML {
+  children = {
+    label: new HTML(),
+    input: new nInput(),
+  }
+
+  state = {
+    label: '',
+    value: '',
+    params: getParams()
+  }
+
+  constructor(label = '', value = '') {
+    super()
+    this.state.label = label
+    this.state.value = value
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.append(this.getLabel())
+    this.append(this.getInput())
+  }
+
+  getLabel() {
+    this.children.label.setText(this.state.label)
+    // this.children.label.setStyle('margin', 'calc(1rem / 4) 0')
+    return this.children.label
+  }
+
+  getInput() {
+    this.children.input.setValue(this.state.value)
+    this.children.input.setPlaceholder(this.state.label)
+    this.children.input.setStyle('background-color', this.state.params.bgcolor)
+    this.children.input.setStyle('box-shadow', '0rem 0rem 0rem 1px #000000')
+    this.children.input.setStyle('color', this.state.params.color)
+    // this.children.input.setStyle('margin', 'calc(1rem / 4) 0')
+    this.children.input.setStyle('box-sizing', 'border-box')
+    this.children.input.setStyle('display', 'inline-block')
+    this.children.input.setStyle('border', 'none')
+    this.children.input.setStyle('width', '100%')
+    return this.children.input
+  }
+
+  getValue() {
+    return this.children.input.getValue()
+  }
+}
