a014485b1b99dde3f2d97940ca8e8ae0748bc7b0
Tarsis Lima <br.tmvdl@gmail.com>
2024-09-24 10:50:13 +0000

    Updating app files

---
+++ projects/screen-capture-api/README.md
@@ -0,0 +1,13 @@
+# http://localhost:8080/screen-capture-api/index.html
+
+[Using the Screen Capture API](https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture#examples)
+
+[MediaDevices: getDisplayMedia() method](https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getDisplayMedia)
+
+[MediaRecorder: MediaRecorder() constructor](https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/MediaRecorder)
+
+[Using the MediaStream Recording API](https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Using_the_MediaStream_Recording_API)
+
+[How video streaming works on the web: An introduction](https://medium.com/canal-tech/how-video-streaming-works-on-the-web-an-introduction-7919739f7e1)
+
+[Support for ISOBMFF-based MIME types in Browsers](https://cconcolato.github.io/media-mime-support/)
---
+++ projects/screen-capture-api/index.js
@@ -0,0 +1,99 @@
+import { HTML, nVideo } from '../../assets/js/libs/frontend/index.js'
+import { TwoColumnsComponent } from '../../assets/js/components/two.columns.component.js'
+import { PaddingComponent } from '../../assets/js/components/padding.component.js'
+import { ButtonComponent } from '../../assets/js/components/button.component.js'
+import { TextComponent } from '../../assets/js/components/text.component.js'
+
+import { GOOGLE, discovery } from '../../assets/js/utils/googleusercontent.js'
+import { datetime2str } from '../../assets/js/utils/datetime.js'
+import * as API from '../../assets/js/utils/api.js'
+import { API_KEY } from './config.js'
+
+export class Page extends PaddingComponent {
+  children = {
+    videoElem: new nVideo(),
+    log: new HTML(),
+  }
+
+  state = {
+    displayMedia: { video: true, audio: true },
+    src: null,
+    chunks: [],
+    apiKey: API_KEY,
+    broadcastId: '',
+    streamId: '',
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.append(new TextComponent({ text: 'screen capture api' }))
+    this.append(new TwoColumnsComponent({ html1: this.getLeft(), html2: this.getRight(), }))
+  }
+
+  getLeft() {
+    const html = new HTML()
+    html.append(new TextComponent({ text: 'capture' }))
+    html.append(new ButtonComponent({ text: 'start capture', onclick: () => this.startCapture() }),)
+    html.append(new ButtonComponent({ text: 'stop capture', onclick: () => this.stopCapture() }),)
+    html.append(new TextComponent({ text: 'gapi' }))
+    html.append(new ButtonComponent({ text: 'load gapi', onclick: () => this.loadGAPI() }),)
+    html.append(new TextComponent({ text: 'youtube api' }))
+    html.append(new ButtonComponent({ text: 'insert', onclick: () => this.insert() }),)
+    html.append(new ButtonComponent({ text: 'execute', onclick: () => this.execute() }),)
+    return html
+  }
+
+  getRight() {
+    const html = new HTML()
+    html.append(this.getVideoElem())
+    html.append(this.getLog())
+    return html
+  }
+
+  getVideoElem() {
+    return this.children.videoElem
+  }
+
+  getLog() {
+    return this.children.log
+  }
+
+  startCapture() {
+    const self = this
+    self.children.log.clear()
+
+    navigator.mediaDevices.getDisplayMedia(self.state.displayMedia)
+      .then((src) => self.state.src = (src))
+      .then(() => console.log('state.src', self.state.src))
+      .then(() => self.children.videoElem.setSrcObject(self.state.src))
+      .then(() => self.children.videoElem.play())
+      .catch((err) => console.error(err))
+  }
+
+  stopCapture() {
+    this.children.videoElem.getSrcObject().getTracks().forEach((t) => t.stop())
+    this.children.videoElem.setSrcObject(null)
+  }
+
+  loadGAPI() {
+    gapi.client.setApiKey(GOOGLE.api_key)
+    gapi.client.load(discovery).then(console.log).catch(console.error)
+  }
+
+  execute() {
+    const b = { id: this.state.broadcastId, part: ['snippet'], streamId: this.state.streamId }
+    return new Promise((s, f) => gapi.client.youtube.liveBroadcasts.bind(b).then(s).catch(f))
+  }
+
+  insert() {
+    const scheduledStartTime = datetime2str()
+    const params = { part: 'id,snippet,contentDetails,status', }
+
+    const body = {
+      snippet: { title: `youtube.livebroadcasts-${scheduledStartTime}`, scheduledStartTime },
+      status: { privacyStatus: 'unlisted' }
+    }
+
+    API.rest.youtube.v3.liveBroadcasts.insert(params, body).then(console.log).catch(console.error)
+  }
+}
