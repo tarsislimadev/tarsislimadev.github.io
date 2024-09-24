ec4b9b6881cfe403504d382720f23a2e530cd0b1
Tarsis Lima <br.tmvdl@gmail.com>
2024-09-24 10:50:14 +0000

    Updating app files

---
+++ projects/websocket/components/content.component.js
@@ -0,0 +1,35 @@
+import { HTML } from '../../../assets/js/libs/frontend/index.js'
+import { TextComponent } from '../../../assets/js/components/text.component.js'
+import * as str from '../../../assets/js/utils/str.js'
+
+export class ContentComponent extends HTML {
+  children = {
+    messages: new HTML(),
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.setStyles()
+    this.append(this.getMessages())
+  }
+
+  setStyles() {
+    this.setStyle('padding', '1rem')
+  }
+
+  getMessages() {
+    return this.children.messages
+  }
+
+  addMessage(header, ...messages) {
+    this.children.messages.prepend(this.createMessageCard(header, ...messages))
+  }
+
+  createMessageCard(header, ...messages) {
+    const card = new HTML()
+    card.append(new TextComponent(header))
+    Array.from(messages).map((message) => card.append(new TextComponent(message)))
+    Array.from([Date.now()]).map((footer) => card.append(new TextComponent(footer, str.timestamp2str(footer))))
+    return card
+  }
+}
---
+++ projects/websocket/index.js
@@ -0,0 +1,71 @@
+import { HTML } from '../../assets/js/libs/frontend/index.js'
+import { HeaderComponent } from './components/header.component.js'
+import { ContentComponent } from './components/content.component.js'
+import { FooterComponent } from './components/footer.component.js'
+
+export class Page extends HTML {
+  children = {
+    header: new HeaderComponent(),
+    content: new ContentComponent(),
+    footer: new FooterComponent(),
+  }
+
+  state = {
+    socket: null,
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.append(this.getHeaderComponent())
+    this.append(this.getFooterComponent())
+    this.append(this.getContentComponent())
+  }
+
+  getHeaderComponent() {
+    this.children.header.addEventListener('connect', (ev) => this.onConnect(ev))
+    return this.children.header
+  }
+
+  onConnect(ev) {
+    this.state.socket = new WebSocket(ev.value)
+    this.state.socket.addEventListener('open', (data) => this.onConnectOpen(data))
+    this.state.socket.addEventListener('message', (data) => this.onConnectMessage(data))
+    this.state.socket.addEventListener('error', (data) => this.onConnectError(data))
+    this.state.socket.addEventListener('close', (data) => this.onConnectClose(data))
+  }
+
+  onConnectOpen(data) {
+    this.children.content.addMessage('open', '')
+  }
+
+  onConnectMessage({ data } = {}) {
+    this.addMessage(data.toString())
+  }
+
+  onConnectError(data) {
+    this.children.content.addMessage('error', data.message.toString())
+  }
+
+  onConnectClose(data) {
+    this.children.content.addMessage('close', '')
+  }
+
+  getContentComponent() {
+    return this.children.content
+  }
+
+  getFooterComponent() {
+    this.children.footer.addEventListener('send', (ev) => this.onSend(ev))
+    return this.children.footer
+  }
+
+  onSend({ value: data } = {}) {
+    this.addMessage(data.toString())
+    this.state.socket.send(data)
+  }
+
+  addMessage(message) {
+    this.children.content.addMessage('message', message.toString())
+  }
+
+}
