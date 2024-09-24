d25fa7c8099dab1095de1cb6d2e88fa43481e682
Tarsis Lima <br.tmvdl@gmail.com>
2024-09-24 10:50:12 +0000

    Updating app files

---
+++ projects/piano/colors.js
@@ -0,0 +1,10 @@
+
+const COLORS = {
+  WHITE: 'rgb(255, 255, 255)',
+  YELLOW: 'rgb(255, 255, 0)',
+  RED: 'rgb(255, 0, 0)',
+  BLACK: 'rgb(0, 0, 0)',
+  GRAY: 'rgb(153, 153, 153)',
+}
+
+export default COLORS
