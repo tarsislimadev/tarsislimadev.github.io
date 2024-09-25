5f2fe9dd51222171a65fb05b65810ba133848dc3
Tarsis Lima <br.tmvdl@gmail.com>
2024-09-25 10:44:31 +0000

    Updating app files

--- projects/twitch/authorize.sh
+++ projects/twitch/authorize.sh
@@ -2,6 +2,6 @@
 
 client_id=""
 
-redirect_uri="https://brtmvdl.github.io/projects/twitch/"
+redirect_uri="https://tarsislimadev.github.io/projects/twitch/"
 
 echo "https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=chat%3Aread+chat%3Aedit"
