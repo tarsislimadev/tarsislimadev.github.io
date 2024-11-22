ab8436c35f5eff9f9a0dec3a8daa6dd043041f7d
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-22 14:19:36 +0000

    Updating app files

--- projects/youtube-live-streaming-api/index.js
+++ projects/youtube-live-streaming-api/index.js
@@ -2,7 +2,7 @@ import { PaddingComponent } from '../../assets/js/components/padding.component.j
 import { ButtonComponent } from '../../assets/js/components/button.component.js'
 import { TextComponent } from '../../assets/js/components/text.component.js'
 import { InputComponent } from '../../assets/js/components/input.component.js'
-import { google } from '../../config.js'
+import { client_id } from '../../assets/js/utils/googleusercontent/client_id.js'
 
 export class Page extends PaddingComponent {
   children = {
@@ -14,30 +14,18 @@ export class Page extends PaddingComponent {
     this.append(new TextComponent({ text: 'youtube live streaming api' }))
     this.append(new ButtonComponent({ text: 'load auth2', onclick: () => this.onLoadAuth2() }))
     this.append(new ButtonComponent({ text: 'auth2 init', onclick: () => this.onAuth2Init() }))
-    this.append(new ButtonComponent({ text: 'client set api key', onclick: () => this.onClientSetApiKey() }))
-    this.append(new ButtonComponent({ text: 'client load youtube', onclick: () => this.onClientLoadYoutube() }))
-    this.append(new ButtonComponent({ text: 'auth2 sign in', onclick: () => this.onAuth2SignIn() }))
   }
 
   onLoadAuth2() {
-    gapi.load('auth2', () => console.log('load auth2'))
+    gapi.load('auth2', () => this.append(new TextComponent({ text: 'load auth2' })))
+  }
+
+  onGoogleAccountsIdInitialize(...data) {
+    this.append(new TextComponent({ text: JSON.stringify(data, null, 4) }))
   }
 
   onAuth2Init() {
-    gapi.auth2.init({ client_id: google.client_id })
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err))
-  }
-
-  onClientSetApiKey() {
-    gapi.client.setApiKey(google.api_key)
-  }
-
-  onClientLoadYoutube() {
-    gapi.client.load('youtube', 'v3', () => console.log('client.load youtube.v3'))
-  }
-
-  onAuth2SignIn() {
-    gapi.auth2.getAuthInstance().signIn({ scope: 'https://www.googleapis.com/auth/youtube.force-ssl' })
+    google.accounts.id.initialize({ client_id, callback: this.onGoogleAccountsIdInitialize.bind(this) })
+    google.accounts.id.prompt()
   }
 }
