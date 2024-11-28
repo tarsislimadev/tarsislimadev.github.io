31a3a29793d80d321e87df510522e23771e8baaf
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-11-28 16:29:19 -0300

    Updating app files

--- en-us/docs/ffmpeg/record.sh
+++ en-us/docs/ffmpeg/record.sh
@@ -1,7 +1,11 @@
+
+
 ffmpeg \
+  -f v4l2 -i /dev/video0 \
   -f x11grab -i :0 \
   -f alsa -i "" \
-  -f v4l2 -i /dev/video0 \
-  -map 0:v -f mp4 x11grab.1.mp4 \
-  -map 1:a -f mp3 alsa.1.mp3 \
-  -map 2:v -f mp4 v4l2.1.mp4 
+  -map 0:v -f mp4 v4l2.1.mp4 \
+  -map 1:v -f mp4 x11grab.1.mp4 \
+  -map 2:a -f mp3 alsa.1.mp3 
+
+
