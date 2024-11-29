84a570b43c53c6eff35f7df2946906940bae796a
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-29 15:31:54 +0000

    Updating app files

--- assets/js/utils/peer.js
+++ assets/js/utils/peer.js
@@ -1,38 +1,55 @@
 import { Peer } from '../../../assets/js/libs/peerjs/index.js'
 import { qrcode } from './functions.js'
 
-const image = document.createElement('img')
-
 class MyPeer extends Peer {
-  removeQRCode() {
-    document.body.removeChild(image)
+  state = {
+    project: null,
+  }
+
+  children = {
+    image: document.createElement('img'),
+  }
+
+  setProject(project) {
+    this.state.project = project
+    return this
+  }
+
+  hideQRCode() {
+    document.body.removeChild(this.children.image)
+    return this
+  }
+
+  showQRCode(url = this.getQRCodeUrl()) {
+    const link = document.createElement('a')
+    link.style.position = 'fixed'
+    link.style.bottom = '1rem'
+    link.style.left = '1rem'
+    link.target = '_blank'
+    link.href = url
+    link.append(this.getQRCodeImage(url))
+    document.body.append(link)
+    return this
+  }
+
+  getQRCodeImage(url = '') {
+    this.children.image.src = qrcode(url)
+    return this.children.image
+  }
+
+  getQRCodeUrl() {
+    const url = new URL(window.location)
+    url.pathname = `/projects/${this.state.project}/controls.html?id=${this.id}`
+    url.search = ''
+    return (url.toString()).replace('%3F', '?')
   }
 }
 
-export const getPeerConnection = () => { }
-
-export const getControlsUrl = (project, id) => {
-  const url = new URL(window.location)
-  url.pathname = `/projects/${project}/controls.html?id=${id}`
-  url.search = ''
-  return (url.toString()).replace('%3F', '?')
-}
-
-export const createControlsLink = (project, id) => {
-  const _url = getControlsUrl(project, id)
-  image.src = qrcode(_url)
-  const link = document.createElement('a')
-  link.style.position = 'fixed'
-  link.style.bottom = '1rem'
-  link.style.left = '1rem'
-  link.target = '_blank'
-  link.href = _url
-  link.append(image)
-  document.body.append(link)
-}
+export const createJustNewPeer = (project) => (new MyPeer()).setProject(project)
 
 export const createNewPeer = (project, qrcode = false) => {
   const peer = new MyPeer()
+  peer.setProject(project)
 
   peer.on('connection', (conn) => {
     console.log('peer connection', { peer, conn })
@@ -56,7 +73,6 @@ export const createNewPeer = (project, qrcode = false) => {
 
   peer.on('open', (open) => {
     console.log('peer open', { peer, open })
-    if (qrcode) createControlsLink(project, peer.id)
   })
 
   peer.on('error', (error) => {
