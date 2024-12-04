435dd339bd5459bd877ace0455cc15b8c1814a0a
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-04 19:29:32 -0300

    Updating app files

---
+++ en-us/docs/ffmpeg/record.sh
@@ -0,0 +1,9 @@
+
+datetime=$( date +%Y%m%d%H%M%S )
+
+ffmpeg -y \
+  -f v4l2 -i /dev/video0 \
+  -f x11grab -i :0 \
+  -f alsa -i "" \
+  -map 0:v -map 2:a -filter:a "volume=volume=+20dB" -f mp4 "v4l2+alsa.${datetime}.mp4" \
+  -map 1:v -f mp4 "x11grab.${datetime}.mp4"
