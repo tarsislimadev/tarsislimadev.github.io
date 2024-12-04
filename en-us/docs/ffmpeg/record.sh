3a5dc97ab616ba54d78343b44a315c9f96dd8a6e
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2024-12-04 16:49:36 -0300

    Updating app files

--- en-us/docs/ffmpeg/record.sh
+++ en-us/docs/ffmpeg/record.sh
@@ -1,11 +1,9 @@
 
+datetime=$( date +%Y%m%d%H%M%S )
 
 ffmpeg \
   -f v4l2 -i /dev/video0 \
   -f x11grab -i :0 \
   -f alsa -i "" \
-  -map 0:v -f mp4 v4l2.1.mp4 \
-  -map 1:v -f mp4 x11grab.1.mp4 \
-  -map 2:a -f mp3 alsa.1.mp3 
-
-
+  -map 0:v -map 2:a -f mp4 "v4l2+alsa.${datetime}.mp4" \
+  -map 1:v -f mp4 "x11grab.${datetime}.mp4"
