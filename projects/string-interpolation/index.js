d9d528a0dafa70c462a0621a9f3fd353d70653eb
Tarsis Lima <br.tmvdl@gmail.com>
2024-12-12 19:15:05 +0000

    Updating app files

---
+++ projects/string-interpolation/index.js
@@ -0,0 +1,53 @@
+import { HTML } from '../../assets/js/libs/frontend/index.js'
+import { PageComponent } from '../../assets/js/components/page.component.js'
+import { TextComponent } from '../../assets/js/components/text.component.js'
+import { InputComponent } from '../../assets/js/components/input.component.js'
+
+const DEFAULT_TEXT = '______'
+
+export class Page extends PageComponent {
+  children = {
+    input_text: new InputComponent({ placeholder: 'text' }),
+    text: new TextComponent(),
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.append(new TextComponent({ text: 'string interpolation' }))
+    this.append(this.getBoxComponent())
+    this.updateText()
+  }
+
+  getBoxComponent() {
+    const html = new HTML()
+
+    html.append(this.getInputText())
+    html.append(this.getText())
+
+    html.setStyle('max-width', '20rem')
+    html.setStyle('margin', '2rem auto')
+
+    return html
+  }
+
+  getInputText() {
+    this.children.input_text.children.input.addEventListener('keyup', () => this.updateText())
+    return this.children.input_text
+  }
+
+  updateText() {
+    this.setText(this.children.input_text.getValue())
+  }
+
+  setText(text = DEFAULT_TEXT) {
+    this.children.text.setText(this.createText(text))
+  }
+
+  createText(text = DEFAULT_TEXT) {
+    return `The text in input is: ${text || DEFAULT_TEXT}. Edit it!`
+  }
+
+  getText() {
+    return this.children.text
+  }
+}
