712c34370cfefd6bfff7ea48b00fafcb63ccb920
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-11 12:04:09 -0300

    Updating app files

--- assets/js/config/firebase/index.js
+++ assets/js/config/firebase/index.js
@@ -1,1 +1,7 @@
-export const a = 'a'
+export const apiKey = 'AIzaSyAdEUEpTK6ASpttnO6XXuDJTlzpXMdk-WU'
+
+export const databaseURL = 'https://tarsislimadev-default-rtdb.firebaseio.com'
+
+export const projectId = 'tarsislimadev'
+
+export const appId = '1:546714325016:web:23ac669c44df761204336d'
