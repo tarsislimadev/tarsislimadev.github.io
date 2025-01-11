88645fc748c39928a89d571395f1ce3d5eb8f05f
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-11 12:03:08 -0300

    Updating app files

--- pages/firebase/index.js
+++ pages/firebase/index.js
@@ -1,40 +1,38 @@
+import { initializeApp } from '../../assets/js/apis/firebase/app/index.js'
+import { getFirestore, collection, getDocs } from '../../assets/js/apis/firebase/firestore/index.js'
+import { getDatabase, ref, set } from '../../assets/js/apis/firebase/database/index.js'
 import { PaddingComponent } from '../../assets/js/components/padding.component.js'
-import { InputComponent } from '../../assets/js/components/input.component.js'
 import { ButtonComponent } from '../../assets/js/components/button.component.js'
-import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-storage.js'
-import * as firebaseConfig from '../../assets/js/utils/firebaseConfig/index.js'
+import { InputComponent } from '../../assets/js/components/input.component.js'
+import * as firebase from '../../assets/js/config/firebase/index.js'
 
 export class Page extends PaddingComponent {
   children = {
     email_input: new InputComponent({ label: 'e-mail' }),
-    whatsapp_input: new InputComponent({ label: 'e-mail' }),
     send_button: new ButtonComponent({ text: 'send', onclick: () => this.onSendButtonClick() })
   }
 
   state = {
     app: null,
     db: null,
+    database: null,
   }
 
   onCreate() {
     super.onCreate()
     this.append(this.getEmailInputComponent())
-    this.append(this.getWhatsAppInputComponent())
     this.append(this.getSendButtonComponent())
     this.init()
   }
 
   init() {
-    this.state.app = initializeApp(firebaseConfig)
-    this.state.db = getFirestore(app)
+    this.state.app = initializeApp(firebase)
+    this.state.db = getFirestore(this.state.app)
+    this.state.database = getDatabase(this.state.app)
   }
 
   getEmailInputComponent() {
-    return this.children.email
-  }
-
-  getWhatsAppInputComponent() {
-    return this.children.whatsapp_input
+    return this.children.email_input
   }
 
   getSendButtonComponent() {
@@ -42,14 +40,18 @@ export class Page extends PaddingComponent {
   }
 
   onSendButtonClick() {
-    this.getCities()
-    alert('send')
+    this.saveData()
   }
 
-  async getCities() {
-    const citiesCol = collection(this.state.db, 'cities')
-    const citySnapshot = await getDocs(citiesCol)
-    const cityList = citySnapshot.docs.map(doc => doc.data())
-    console.log({ cityList })
+  saveData() {
+    const datetime = Date.now().toString()
+
+    const data1 = { email: this.children.email_input.children.input.getValue(), datetime }
+
+    const ref1 = ref(this.state.database, 'firebase/' + datetime)
+
+    set(ref1, data1).then(() => {
+      console.log('Document successfully written!')
+    })
   }
 }
