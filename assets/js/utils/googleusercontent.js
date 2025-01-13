3bce884ce147a6ccdceaa3debab3e5b36b82028e
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-13 18:01:17 -0300

    Updating app files

--- assets/js/utils/googleusercontent.js
+++ assets/js/utils/googleusercontent.js
@@ -2,19 +2,11 @@ import api_key from './googleusercontent/api_key.js'
 
 import scope from './googleusercontent/scopes/index.js'
 
-// import discovery from './googleusercontent/discovery.js'
-
 import response_type from './googleusercontent/response_type.js'
 
 import client_id from './googleusercontent/client_id.js'
 
 import project_id from './googleusercontent/project_id.js'
-
-// import auth_uri from './googleusercontent/auth_uri.js'
-
-// import token_uri from './googleusercontent/token_uri.js'
-
-// import auth_provider_x509_cert_url from './googleusercontent/auth_provider_x509_cert_url.js'
 
 import redirect_uri from './googleusercontent/redirect_uri.js'
 
