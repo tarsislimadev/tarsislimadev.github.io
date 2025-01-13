83403b945092df6cc8c504720821601c4f8f5c2b
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-13 16:51:42 -0300

    Updating app files

---
+++ assets/js/config/googleusercontent/index.js
@@ -0,0 +1,19 @@
+import api_key from './api_key.js'
+
+import auth_provider_x509_cert_url from './auth_provider_x509_cert_url.js'
+
+import auth_uri from './auth_uri.js'
+
+import client_id from './client_id.js'
+
+import client_secret from './client_secret.js'
+
+import project_id from './project_id.js'
+
+import redirect_uri from './redirect_uri.js'
+
+import response_type from './response_type.js'
+
+import token_uri from './token_uri.js'
+
+export default { api_key, auth_provider_x509_cert_url, auth_uri, client_id, client_secret, project_id, redirect_uri, response_type, token_uri, }
