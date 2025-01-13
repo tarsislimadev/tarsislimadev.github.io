8230f2c119ee1eaf3b656a6574d6ec5dff3dcbc9
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-13 16:15:42 -0300

    Updating app files

--- assets/js/apis/binance.js
+++ assets/js/apis/binance.js
@@ -5,3 +5,7 @@ export const websocket = {
   url: 'wss://ws-api.binance.com:443/ws-api/v3',
   docs: 'https://developers.binance.com/docs/binance-spot-api-docs/web-socket-api',
 }
+
+export class BinanceWebSocket extends WebSocket {
+  constructor() { super(websocket.url) }
+}
