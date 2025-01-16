39a7cb38f26c6070c53c949462e0afc58c56d2ba
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-16 18:04:02 -0300

    Updating app files

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 import { HTML } from '../assets/js/libs/afrontend/index.js'
-import { ProjectThumbnailComponent } from './assets/js/components/project.thumbnail.component.js'
+import { ThumbnailComponent } from './assets/js/components/thumbnail.component.js'
 import { PaddingComponent } from './assets/js/components/padding.component.js'
 import { TextComponent } from './assets/js/components/text.component.js'
-import experiences from './assets/js/lists/experiences.js'
+
+import projects from './assets/js/lists/projects.js'
+import products from './assets/js/lists/products.js'
+import posts from './assets/js/lists/posts.js'
+
+class ProjectThumbnailComponent extends ThumbnailComponent { }
+class ProductThumbnailComponent extends ThumbnailComponent { }
+class PostThumbnailComponent extends ThumbnailComponent { }
 
 export class Page extends PaddingComponent {
   onCreate2() {
-    this.append(new TextComponent({ text: 'projects' }))
-    const projects = new HTML()
-    Array.from(experiences).filter(({ step }) => step >= 3).map(({ id }) => projects.append(new ProjectThumbnailComponent({ id })))
-    this.append(projects)
+    const html = new HTML()
 
-    this.append(new TextComponent({ text: 'posts' }))
-    const posts = new HTML()
-    Array.from(experiences).filter(({ step }) => step >= 3).map(({ id }) => posts.append(new ProjectThumbnailComponent({ id })))
-    this.append(posts)
+    html.append(new TextComponent({ text: 'projects' }))
+    Array.from(projects).map((project) => html.append(new ProjectThumbnailComponent(project)))
+
+    html.append(new TextComponent({ text: 'products' }))
+    Array.from(products).map((product) => html.append(new ProductThumbnailComponent(product)))
+
+    html.append(new TextComponent({ text: 'posts' }))
+    Array.from(posts).map((post) => html.append(new PostThumbnailComponent(post)))
+
+    this.append(html)
   }
 }
