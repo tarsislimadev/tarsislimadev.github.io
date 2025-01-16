04f031a3bc71fdaa977ca641f112e47551875437
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-16 17:36:10 -0300

    Updating app files

--- assets/js/components/padding.component.js
+++ assets/js/components/padding.component.js
@@ -1,11 +1,21 @@
 import { HTML } from '../libs/afrontend/index.js'
 import { HeaderComponent } from './header.component.js'
+import { FooterComponent } from './footer.component.js'
 
 export class PaddingComponent extends HTML {
   onCreate() {
     super.onCreate()
-    this.setStyle('padding', 'calc(1rem / 4)')
-    this.setStyle('margin', '0 auto')
+    this.setStyles()
     this.append(new HeaderComponent())
+    this.onCreate2()
+    this.append(new FooterComponent())
   }
+
+  setStyles() {
+    this.setStyles('padding', 'calc(1rem / 4)')
+    this.setStyle('margin', '0 auto')
+  }
+
+  onCreate2() { }
+
 }
