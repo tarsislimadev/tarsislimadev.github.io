730c1603e3ab213be657247d44c596664795a407
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-25 19:32:07 -0300

    Updating app files

--- projects/domains/index.js
+++ projects/domains/index.js
@@ -3,7 +3,6 @@ import { getFirestore } from '../../assets/js/apis/firebase/firestore/index.js'
 import { getDatabase, ref, set } from '../../assets/js/apis/firebase/database/index.js'
 import { PaddingComponent } from '../../assets/js/components/padding.component.js'
 import { ButtonComponent } from '../../assets/js/components/button.component.js'
-import { SelectComponent } from '../../assets/js/components/select.component.js'
 import { InputComponent } from '../../assets/js/components/input.component.js'
 import firebase from '../../assets/js/config/firebase/index.js'
 import * as RDAP from '../../assets/js/apis/rdap.js'
@@ -11,7 +10,6 @@ import * as RDAP from '../../assets/js/apis/rdap.js'
 export class Page extends PaddingComponent {
   children = {
     domain_input: new InputComponent({ label: 'domain' }),
-    suffix_input: new SelectComponent({ label: 'suffix', options: [['_com', '.com'], ['_com_br', '.com.br']] }),
     send_button: new ButtonComponent({ text: 'send', onclick: () => this.onSendButtonClick() })
   }
 
@@ -24,14 +22,11 @@ export class Page extends PaddingComponent {
   onCreate() {
     super.onCreate()
     this.append(this.getDomainInputComponent())
-    this.append(this.getSuffixInputComponent())
     this.append(this.getSendButtonComponent())
     this.init()
   }
 
   init() {
-    // console.log({ firebase: firebase.toString() })
-
     this.state.app = initializeApp(firebase)
     this.state.db = getFirestore(this.state.app)
     this.state.database = getDatabase(this.state.app)
@@ -41,31 +36,22 @@ export class Page extends PaddingComponent {
     return this.children.domain_input
   }
 
-  getSuffixInputComponent() {
-    return this.children.suffix_input
-  }
-
   getSendButtonComponent() {
     return this.children.send_button
   }
 
   onSendButtonClick() {
     const domain = this.children.domain_input.children.input.getValue()
-    const suffix = this.children.suffix_input.children.input.getValue()
-    this.findDomain(domain, suffix)
+    this.findDomain(domain)
       .then(json => console.log({ json }))
       .catch(err => console.error(err))
   }
 
-  async findDomain(domain, suffix, datetime = Date.now().toString()) {
-    const json = await RDAP.domain(domain + suffix.replaceAll('_', '.'))
-
-    const data1 = { domain, suffix, json, datetime, location: window.location.toString() }
-
+  async findDomain(domain, datetime = Date.now().toString()) {
+    const json = await RDAP.domain(domain)
+    const data1 = { domain, json, datetime, location: window.location.toString() }
     const ref1 = ref(this.state.database, 'domains/' + datetime)
-
     await set(ref1, data1)
-
-    return json
+    return { json }
   }
 }
