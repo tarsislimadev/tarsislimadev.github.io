59024062b3937091f6f9d8e9c96b26102dad2db2
Tarsis Lima <br.tmvdl@gmail.com>
2025-04-17 10:02:56 -0300

    Implement Google API initialization and update label listing functionality

--- projects/gmail/index.js
+++ projects/gmail/index.js
@@ -5,6 +5,8 @@ import { TextComponent } from '../../assets/js/components/text.component.js'
 
 import * as LOCAL from '../../assets/js/utils/local.js'
 
+import * as GOOGLE from '../../assets/js/config/googleusercontent/_index.js'
+
 import { rest } from '../../assets/js/utils/api.js'
 
 export class Page extends PaddingComponent {
@@ -22,6 +24,7 @@ export class Page extends PaddingComponent {
     super.onCreate()
     this.setEvents()
     this.append(new TextComponent({ text: 'Gmail API v1' }))
+    this.append(new ButtonComponent({ text: 'Google init', onclick: () => this.googleInit() }))
     this.append(new ButtonComponent({ text: 'List labels', onclick: () => this.listLabels() }))
     this.append(this.children.content)
   }
@@ -31,15 +34,38 @@ export class Page extends PaddingComponent {
   }
 
   getAccessToken() {
-    const access_token = LOCAL.get(['google.access_token'])
-    console.log({ access_token })
-    return access_token
+    throw new Error('getAccessToken not implemented')
+  }
+
+  googleInit() {
+    gapi.client.init({
+      apiKey: GOOGLE.API_KEY,
+      clientId: GOOGLE.CLIENT_ID,
+      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'],
+      scope: 'https://www.googleapis.com/auth/gmail.readonly',
+    }).then(() => {
+      console.log('GAPI initialized')
+      this.state.gapiInited = true
+      this.getAccessToken()
+    }
+    ).catch((err) => {
+      console.error('Error initializing GAPI', err)
+    })
   }
 
   listLabels() {
-    rest.gmail.v1.users.getProfile().then(console.log)
-    // gapi.client.gmail.users.labels.list({ 'userId': 'me' })
-    //   .then((res) => console.log(res))
-    //   .catch((err) => console.error(err))
+    if (!this.state.gapiInited) {
+      console.error('GAPI not initialized')
+      return
+    }
+    gapi.client.gmail.users.labels.list({ 'userId': 'me' })
+      .then((res) => {
+        console.log(res)
+        this.children.content.setText(JSON.stringify(res))
+      })
+      .catch((err) => {
+        console.error(err)
+        this.children.content.setText(JSON.stringify(err))
+      })
   }
 }
