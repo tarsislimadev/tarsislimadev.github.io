14012aaf3f4f1c76afa27ad590829942def83e43
Tarsis Lima <br.tmvdl@gmail.com>
2025-04-18 19:33:23 -0300

    Refactor token management in login and logout processes

--- pages/logout/index.js
+++ pages/logout/index.js
@@ -1,11 +1,11 @@
 import { PaddingComponent } from '../../assets/js/components/padding.component.js'
-import * as LOCAL from '../../assets/js/utils/local.js'
+
 import * as FLOW from '../../assets/js/utils/flow.js'
 
 export class Page extends PaddingComponent {
   onCreate() {
     super.onCreate()
-    LOCAL.set(['access_token'], null)
+    localStorage.clear()
     FLOW.goTo('/?access_token=0')
   }
 }
