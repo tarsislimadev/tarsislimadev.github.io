cbc526a0f1b56d66add0ee5293e22e54083df0ba
Tarsis Lima <br.tmvdl@gmail.com>
2025-04-20 16:00:20 -0300

    Refactor Page class to extend PageComponent and enhance structure with Text and TwoColumns components

--- projects/gmail/index.js
+++ projects/gmail/index.js
@@ -1,7 +1,15 @@
 import { HTML } from '../../assets/js/libs/afrontend/index.js'
+import { PageComponent } from '../../assets/js/components/page.component.js'
+import { TextComponent } from '../../assets/js/components/text.component.js'
+import { TwoColumnsComponent } from '../../assets/js/components/two.columns.component.js'
 
-export class Page extends HTML {
+export class Page extends PageComponent {
   onCreate() {
     super.onCreate()
+    this.append(new TextComponent('Gmail API'))
+    this.append(new TwoColumnsComponent({
+      html1: new TextComponent('Left Column'),
+      html2: new TextComponent('Right Column'),
+    }))
   }
 }
