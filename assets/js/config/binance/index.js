9c791d59b541042028f7fb6bbe23035f557d4ae5
Tarsis Lima <br.tmvdl@gmail.com>
2025-04-29 11:46:03 -0300

    Fix name and URL of Binance APIs

--- assets/js/config/binance/index.js
+++ assets/js/config/binance/index.js
@@ -1,3 +1,3 @@
 export default {
-  websocket: { name: 'Binance Websocket API', docs: 'https://developers.binance.com/docs/binance-spot-api-docs/web-socket-api/general-api-information' }
+  websocket: { name: 'binance websocket api', docs: 'https://developers.binance.com/docs/binance-spot-api-docs/CHANGELOG' }
 }
