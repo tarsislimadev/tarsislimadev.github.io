144fe8af8f7223deb5968e1ab7e94753c16bb057
Tarsis Lima <br.tmvdl@gmail.com>
2025-05-14 04:38:08 -0300

    Add IntervalEnum class to define time intervals in seconds and minutes

---
+++ projects/n8n/enums/interval.js
@@ -0,0 +1,12 @@
+export class IntervalEnum {
+  static SECONDS_1 = 1
+  static SECONDS_2 = 2
+  static SECONDS_3 = 3
+  static SECONDS_5 = 5
+  static SECONDS_10 = 10
+  static SECONDS_15 = 15
+  static SECONDS_20 = 20
+  static SECONDS_30 = 30
+  static SECONDS_45 = 45
+  static MINUTES_1 = 60
+}
