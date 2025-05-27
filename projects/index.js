12f86efbf897b0b31574c1c7a8292b8baeb758bd
Tarsis Lima <br.tmvdl@gmail.com>
2025-05-27 16:48:56 -0300

    Refactor Page component to extend PageComponent and update project list handling

--- projects/index.js
+++ projects/index.js
@@ -1,29 +1,31 @@
-import { HTML } from '../assets/js/libs/afrontend/index.js'
+import { HTML, nSpan, nLink, nImage } from '../assets/js/libs/afrontend/index.js'
 import { ThumbnailComponent } from '../assets/js/components/thumbnail.component.js'
-import { PaddingComponent } from '../assets/js/components/padding.component.js'
-import { TextComponent } from '../assets/js/components/text.component.js'
+import { PageComponent } from '../assets/js/components/page.component.js'
 
 import projects from '../assets/js/lists/projects.js'
-import products from '../assets/js/lists/products.js'
-import posts from '../assets/js/lists/posts.js'
 
 class ProjectThumbnailComponent extends ThumbnailComponent { }
-class ProductThumbnailComponent extends ThumbnailComponent { }
-class PostThumbnailComponent extends ThumbnailComponent { }
 
-export class Page extends PaddingComponent {
-  onCreate2() {
+class ProjectsListComponent extends HTML {
+  updateProjectsList(list = []) {
+    this.clear()
+
+    if (list.length === 0) {
+      this.setText('No projects found')
+      return
+    }
+
+    Array.from(list).map((project) => this.append(new ProjectThumbnailComponent(project)))
+  }
+}
+
+export class Page extends PageComponent {
+  projectsList = new ProjectsListComponent()
+
+  getBodyComponent() {
     const html = new HTML()
-
-    html.append(new TextComponent({ text: 'projects' }))
-    Array.from(projects).map((project) => html.append(new ProjectThumbnailComponent(project)))
-
-    html.append(new TextComponent({ text: 'products' }))
-    Array.from(products).map((product) => html.append(new ProductThumbnailComponent(product)))
-
-    html.append(new TextComponent({ text: 'posts' }))
-    Array.from(posts).map((post) => html.append(new PostThumbnailComponent(post)))
-
-    this.append(html)
+    html.append(this.projectsList)
+    this.projectsList.updateProjectsList(projects)
+    return html
   }
 }
