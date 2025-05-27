f3f69ce0a5b402882041c4f6bd2b1e63f2c3165f
Tarsis Lima <br.tmvdl@gmail.com>
2025-05-27 16:42:56 -0300

    Refactor PageComponent to extract style setting into a separate method

--- assets/js/components/page.component.js
+++ assets/js/components/page.component.js
@@ -5,9 +5,15 @@ import { FooterComponent } from './footer.component.js'
 export class PageComponent extends HTML {
   onCreate() {
     super.onCreate()
+    this.setStyles()
     if (this.hasHeader()) this.append(new HeaderComponent())
     this.append(this.getBodyComponent())
     if (this.hasFooter()) this.append(new FooterComponent())
+  }
+
+  setStyles() {
+    this.setStyle('padding', '0.5rem')
+    this.setStyle('margin', '0 auto')
   }
 
   getBodyComponent() {
