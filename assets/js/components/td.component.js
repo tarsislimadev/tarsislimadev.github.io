fa48a2ab8e4868e908ea06e2128da8a9d99d9611
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-05 21:22:28 -0300

    Implement TdComponent with constructor for text and styles, and methods for creation and style application

--- assets/js/components/td.component.js
+++ assets/js/components/td.component.js
@@ -1,3 +1,23 @@
 import { HTML, nTd } from '../../../assets/js/libs/afrontend/index.js'
 
-export class TdComponent extends nTd { }
+export class TdComponent extends nTd {
+  styles = {}
+  text = ''
+
+  constructor({ text = '', styles = {} } = {}) {
+    super()
+    this.text = text
+    this.styles = styles
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.setText(this.text)
+    this.setStyles()
+  }
+
+  setStyles() {
+    const self = this
+    Object.keys(self.styles).map((key) => self.setStyle(key, self.styles[key]))
+  }
+}
