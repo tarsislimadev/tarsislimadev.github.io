75f180b1bfd822dc8325c7ccd08f032d28b767ff
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-05 22:39:23 -0300

    Add dispatchWindowEvent function to create and dispatch custom events

--- assets/js/utils/window.js
+++ assets/js/utils/window.js
@@ -5,3 +5,9 @@ export const getWidth = () => window.innerWidth
 export const getHeight = () => window.innerHeight
 
 export const getAspect = () => getWidth() > getHeight() ? getWidth() / getHeight() : getHeight() / getWidth()
+
+export const dispatchWindowEvent = (name, value = null) => {
+  const ev = new CustomEvent(name)
+  ev.value = value
+  window.dispatchEvent(ev)
+}
