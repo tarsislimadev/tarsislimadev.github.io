b8949b909c20bb935b95e14e39d860e3f31be6d0
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-10 17:18:46 -0300

    Add BinanceSpotApiV3 class with methods for fetching ticker prices

--- assets/js/apis/binance.js
+++ assets/js/apis/binance.js
@@ -9,3 +9,21 @@ export const websocket = {
 export class BinanceWebSocket extends WebSocket {
   constructor() { super(websocket.url) }
 }
+
+const createURLParams = (params) => params ? ('?' + (new URLSearchParams(params)).toString()) : ''
+
+const createURL = (hostAndPath, params = {}) => new URL(hostAndPath + createURLParams(params)).toString()
+
+const getJSON = (url, params = {}) => fetch(createURL(url, params)).then(res => res.json())
+
+export class BinanceSpotApiV3 {
+  static BASE_URL = 'https://api1.binance.com/api/v3'
+
+  static getJSON(path, params = {}) {
+    return getJSON(BinanceSpotApiV3.BASE_URL + '/' + path, params)
+  }
+
+  static getTickerPrice({ symbols = [] } = {}) {
+    return BinanceSpotApiV3.getJSON('ticker/price', { symbols: `[${symbols.map((s) => `"${s}"`).join(',')}]` })
+  }
+}
