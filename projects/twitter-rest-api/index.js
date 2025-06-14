f0c3be7213d1c1fcecb84e61e36b554d72903e6d
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-14 00:10:13 -0300

    Refactor Page component to eliminate 'children' object, using direct property references for improved clarity and maintainability

--- projects/twitter-rest-api/index.js
+++ projects/twitter-rest-api/index.js
@@ -5,16 +5,14 @@ import { FormComponent } from '../../assets/js/components/form.component.js'
 // https://developer.twitter.com/en/docs/authentication/oauth-2-0/authorization-code
 
 export class Page extends HTML {
-  children = {
-    ResponseType: new nInputTextGroup(),
-    ClientId: new nInputTextGroup(),
-    RedirectUri: new nInputTextGroup(),
-    Scope: new nInputTextGroup(),
-    State: new nInputTextGroup(),
-    CodeChallenge: new nInputTextGroup(),
-    CodeChallengeMethod: new nInputTextGroup(),
-    SubmitButton: new nButton(),
-  }
+  ResponseType = new nInputTextGroup()
+  ClientId = new nInputTextGroup()
+  RedirectUri = new nInputTextGroup()
+  Scope = new nInputTextGroup()
+  State = new nInputTextGroup()
+  CodeChallenge = new nInputTextGroup()
+  CodeChallengeMethod = new nInputTextGroup()
+  SubmitButton = new nButton()
 
   onCreate() {
     super.onCreate()
@@ -43,49 +41,49 @@ export class Page extends HTML {
   }
 
   getResponseType() {
-    this.children.ResponseType.children.label.setText('Response Type')
-    this.children.ResponseType.children.input.setValue('code')
-    return this.children.ResponseType
+    this.ResponseType.label.setText('Response Type')
+    this.ResponseType.input.setValue('code')
+    return this.ResponseType
   }
 
   getClientId() {
-    this.children.ClientId.children.label.setText('Client Id')
-    this.children.ClientId.children.input.setValue('this.children.ClientId')
-    return this.children.ClientId
+    this.ClientId.label.setText('Client Id')
+    this.ClientId.input.setValue('this.ClientId')
+    return this.ClientId
   }
 
   getRedirectUri() {
-    this.children.RedirectUri.children.label.setText('Redirect Uri')
-    this.children.RedirectUri.children.input.setValue(window.location.toString())
-    return this.children.RedirectUri
+    this.RedirectUri.label.setText('Redirect Uri')
+    this.RedirectUri.input.setValue(window.location.toString())
+    return this.RedirectUri
   }
 
   getScope() {
-    this.children.Scope.children.label.setText('Scope')
-    this.children.Scope.children.input.setValue('tweet.read users.read offline.access')
-    return this.children.Scope
+    this.Scope.label.setText('Scope')
+    this.Scope.input.setValue('tweet.read users.read offline.access')
+    return this.Scope
   }
 
   getState() {
-    this.children.State.children.label.setText('State')
-    this.children.State.children.input.setValue('state')
-    return this.children.State
+    this.State.label.setText('State')
+    this.State.input.setValue('state')
+    return this.State
   }
 
   getCodeChallenge() {
-    this.children.CodeChallenge.children.label.setText('Code Challenge')
-    this.children.CodeChallenge.children.input.setValue('challenge')
-    return this.children.CodeChallenge
+    this.CodeChallenge.label.setText('Code Challenge')
+    this.CodeChallenge.input.setValue('challenge')
+    return this.CodeChallenge
   }
 
   getCodeChallengeMethod() {
-    this.children.CodeChallengeMethod.children.label.setText('Code Challenge Method')
-    this.children.CodeChallengeMethod.children.input.setValue('plain')
-    return this.children.CodeChallengeMethod
+    this.CodeChallengeMethod.label.setText('Code Challenge Method')
+    this.CodeChallengeMethod.input.setValue('plain')
+    return this.CodeChallengeMethod
   }
 
   getSubmitButton() {
-    this.children.SubmitButton.setText('Submit')
-    return this.children.SubmitButton
+    this.SubmitButton.setText('Submit')
+    return this.SubmitButton
   }
 }
