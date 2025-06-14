b82edb959029dfc609054f194fc5051c1f05bf3c
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-14 00:01:22 -0300

    Refactor InputComponent and Page component to simplify state management by removing children objects and directly using class properties

--- projects/facebook-rest-api/components/input.component.js
+++ projects/facebook-rest-api/components/input.component.js
@@ -3,11 +3,11 @@ import * as Components from '../../../assets/js/components/input.component.js'
 export class InputComponent extends Components.InputComponent {
   onCreate() {
     super.onCreate()
-    this.children.label.setStyle('padding', 'calc(1rem / 4)')
-    this.children.label.setStyle('margin', '0rem')
-    this.children.input.setStyle('padding', 'calc(1rem / 4)')
-    this.children.input.setStyle('box-sizing', 'border-box')
-    this.children.input.setStyle('margin', '0rem')
-    this.children.input.setStyle('width', '100%')
+    this.label.setStyle('padding', 'calc(1rem / 4)')
+    this.label.setStyle('margin', '0rem')
+    this.input.setStyle('padding', 'calc(1rem / 4)')
+    this.input.setStyle('box-sizing', 'border-box')
+    this.input.setStyle('margin', '0rem')
+    this.input.setStyle('width', '100%')
   }
 }
--- projects/facebook-rest-api/components/inputs.component.js
+++ projects/facebook-rest-api/components/inputs.component.js
@@ -2,7 +2,7 @@ import * as Components from '../../../assets/js/components/inputs.component.js'
 import { InputComponent } from '../../../assets/js/components/input.component.js'
 
 export class InputsComponent extends Components.InputsComponent {
-  children = {
+  components = {
     path: new InputComponent({ label: 'path' }),
     path2: new InputComponent({ label: 'path2' }),
   }
