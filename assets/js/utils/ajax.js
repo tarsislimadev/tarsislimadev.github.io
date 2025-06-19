5148481d773972e61211d29ebe59a0abed0b4269
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-19 03:52:00 -0300

    Remove console.log

--- assets/js/utils/ajax.js
+++ assets/js/utils/ajax.js
@@ -3,8 +3,6 @@ import { Response } from './http.js'
 export const simple_request = ({ headers = [['', ''], ['', ''],], method = 'GET', protocol = '', hostname = '', pathname = '', search = [['', ''], ['', ''], ['', ''],], body = null } = window.location) => request(method, `${protocol}//${hostname}:${protocol == 'http:' ? 80 : 443}/${pathname.replace(/^\//, '')}?` + (new URLSearchParams(search)).toString(), body, headers)
 
 export const request = (method = 'GET', url = '', body = {}, headers = {}) => {
-  console.log('request', { method, url, body, headers })
-
   return new Promise((res, rej) => {
     const xhr = new XMLHttpRequest()
     xhr.open(method, url, true)
