8a7ec32eecd433d71a25a97fae576b68c5846c6b
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-19 03:51:13 -0300

    Remove unused components: TdComponent, TdTextComponent, nTd2, and TopBar

--- projects/exchanges/components/td.text.component.js
+++
@@ -1,9 +0,0 @@
-import { TdComponent } from './td.component.js'
-
-import { TextComponent } from '../../../assets/js/components/text.component.js'
-
-export class TdTextComponent extends TdComponent {
-  constructor(text = '') {
-    super(new TextComponent(text))
-  }
-}
