524a384cad45fcd01b4e393adec6ea15cb60449d
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-20 23:37:21 -0300

    Refactor Page component to streamline imports and update method; enhance readability and maintainability

--- projects/exchanges/index.v20250618.js
+++ projects/exchanges/index.v20250618.js
@@ -1,14 +1,18 @@
 import { PageComponent } from '../../assets/js/components/page.component.js'
+import { UpdatableTextComponent } from '../../assets/js/components/updatable.text.component.js'
 import { BinancePricesTableComponent } from './components/binance.prices.table.component.js'
-import { BinanceBuysTableComponent } from './components/binance.buys.table.component.js'
 import { BinanceSellsTableComponent } from './components/binance.sells.table.component.js'
+import { BinanceBuysTableComponent } from './components/binance.buys.table.component.js'
 import { ColumnComponent } from '../../assets/js/components/column.component.js'
+import { TextComponent } from '../../assets/js/components/text.component.js'
 
 import { getSymbolsList } from './lists/symbols.list.js'
 
 import { dispatchWindowEvent } from '../../assets/js/utils/window.js'
+import { datetime2str } from '../../assets/js/utils/str.js'
 import * as Local from '../../assets/js/utils/local.js'
 import * as API from '../../assets/js/utils/api.js'
+
 import { PriceModel } from '../../assets/js/models/price.model.js'
 
 export class Page extends PageComponent {
@@ -16,18 +20,22 @@ export class Page extends PageComponent {
     super.onCreate()
     Local.set(['binance.symbols'], getSymbolsList())
     this.append(new ColumnComponent([
+      new TextComponent({ text: 'exchanges' }),
+      new UpdatableTextComponent('updateprices', () => datetime2str(new Date()))
+    ]))
+    this.append(new ColumnComponent([
       new BinancePricesTableComponent(),
       new BinanceBuysTableComponent(),
       new BinanceSellsTableComponent(),
     ]))
-    this.updateBinancePrices()
+    this.update()
   }
 
-  updateBinancePrices() {
+  update() {
     API.rest.binance.v3.ticker.getPricesBySymbols(getSymbolsList())
       .then((prices) => Local.set(['binance.prices'], this.parsePrices(prices)))
       .then(() => dispatchWindowEvent('updateprices'))
-      .finally(() => this.updateBinancePrices())
+      .finally(() => this.update())
   }
 
   parsePrices(prices = []) {
