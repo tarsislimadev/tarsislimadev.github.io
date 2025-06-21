660bf371d9c09fba1b9abadcc4d7688841736bec
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-20 23:36:33 -0300

    Refactor BinanceLocal methods to streamline mapping of buys and prices; update getPriceBySymbol to use getSymbol method

--- projects/exchanges/local/binance.local.js
+++ projects/exchanges/local/binance.local.js
@@ -5,23 +5,19 @@ import { PriceModel } from '../../../assets/js/models/price.model.js'
 export class BinanceLocal {
   static getBuys() {
     return Local.get(['binance.buys'], [])
-      .map((b) => {
-        const buy = new BinanceBuyModel()
-        buy.fromJSON(b)
-        return buy
-      })
+      .map((b) => new BinanceBuyModel().fromJSON(b))
   }
 
   static getPrices() {
     return Local.get(['binance.prices'])
-      .map((b) => {
-        const price = new PriceModel()
-        price.fromJSON(b)
-        return price
-      })
+      .map((b) => new PriceModel().fromJSON(b))
   }
 
   static getPriceBySymbol(symbol) {
-    return BinanceLocal.getPrices().find((p) => p.symbol == symbol)
+    return BinanceLocal.getPrices().find((p) => p.getSymbol() == symbol)
+  }
+
+  static addBuy(buy = new BinanceBuyModel()) {
+    Local.add(['binance.buys'], buy)
   }
 }
