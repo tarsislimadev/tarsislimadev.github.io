6fcc438bcef9b4b39bdcf9188458bba2f80db4af
Tarsis Lima <br.tmvdl@gmail.com>
2025-06-24 17:12:32 -0300

    Add Firebase Helper

---
+++ assets/js/helpers/firebase.helper.js
@@ -0,0 +1,35 @@
+import { initializeApp } from '../../../assets/js/apis/firebase/app/index.js'
+import { getDatabase, ref, set, onValue } from '../../../assets/js/apis/firebase/database/index.js'
+import firebase from '../../../assets/js/config/firebase/index.js'
+
+export class FirebaseHelper {
+  app = null
+  database = null
+  directory = null
+
+  constructor(directory) {
+    this.directory = directory
+    this.app = initializeApp(firebase)
+    this.database = getDatabase(this.app)
+  }
+
+  getDirectory() { return this.directory }
+
+  set(...args) { set(...args) }
+
+  getRef(item = null) {
+    return ref(this.database, item ? (this.getDirectory() + '/' + item) : this.getDirectory())
+  }
+
+  save(data, ref1 = this.getRef(Date.now())) { return this.set(ref1, data) }
+
+  list() {
+    return new Promise((resolve, reject) => {
+      onValue(this.getRef(), (snapshot) => {
+        const data = snapshot.val()
+        if (data) resolve(data)
+        else resolve([])
+      }, (error) => reject(error))
+    })
+  }
+}
