f737bd2b6c6a4378db04248f529f62b5a790d623
Tarsis Lima <br.tmvdl@gmail.com>
2025-07-08 15:10:39 +0000

    Add candlestick chart visualization to the klines message card, providing a visual representation of the price data

--- projects/binance-websocket-api/components/output.messages.component.js
+++ projects/binance-websocket-api/components/output.messages.component.js
@@ -2,10 +2,50 @@ import { BinanceMessageCardComponent } from './binance.message.card.component.js
 import { CardBodyComponent } from '../../../assets/js/components/card.body.component.js'
 import { TextComponent } from '../../../assets/js/components/text.component.js'
 
+import { Element } from '../../../assets/js/elements/element.js'
+
+class CandlestickChartsElement extends Element {
+  data = []
+
+  constructor({ data = [] } = {}) {
+    super({})
+    this.data = data
+  }
+
+  onCreate() {
+    const chart = new google.visualization.CandlestickChart(this.element)
+    chart.draw(
+      google.visualization.arrayToDataTable(this.data, true),
+      {
+        legend: 'none',
+        bar: { groupWidth: '100%' }, // Remove space between bars.
+        candlestick: {
+          fallingColor: { strokeWidth: 0, fill: '#a52714' }, // red
+          risingColor: { strokeWidth: 0, fill: '#0f9d58' }   // green
+        }
+      }
+    )
+  }
+}
+
 export class KlinesMessageCardComponent extends BinanceMessageCardComponent {
   getBodyComponent() {
+    const data = this.message.Payload.map(([
+      kline_open_time,
+      open_price,
+      high_price,
+      low_price,
+      close_price
+    ]) => ([
+      new Date(kline_open_time),
+      +low_price,
+      +open_price,
+      +close_price,
+      +high_price,
+    ]))
+
     const body = new CardBodyComponent()
-    body.append(new TextComponent({ text: 'klines output' }))
+    body.append(new CandlestickChartsElement({ data }))
     return body
   }
 }
