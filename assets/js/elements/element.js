4a57b1ab4c629d5cbe3a9ea9080aba057a26bc6e
Tarsis Lima <br.tmvdl@gmail.com>
2025-07-08 15:06:02 +0000

    Add container control to element

--- assets/js/elements/element.js
+++ assets/js/elements/element.js
@@ -1,12 +1,19 @@
 import { HTML } from '../libs/afrontend/index.js'
 
 export class Element extends HTML {
-  constructor({ children = [], attrs = [], styles = {}, container_styles = {}, events = {} } = {}) {
+  containered = true
+
+  constructor({ children = [], attrs = [], styles = {}, container_styles = {}, events = {}, containered = true } = {}) {
     super()
     Array.from(children).map(c => this.append(c))
     Array.from(attrs).map((a) => this.setAttr(a, attrs[a]))
     Object.keys(styles).map(s => this.setStyle(s, styles[s]))
     Object.keys(container_styles).map(s => this.setContainerStyle(s, container_styles[s]))
     Object.keys(events).map((e) => this.addEventListener(e, events[e]))
+    this.containered = containered
+  }
+
+  hasContainer() {
+    return this.containered
   }
 }
