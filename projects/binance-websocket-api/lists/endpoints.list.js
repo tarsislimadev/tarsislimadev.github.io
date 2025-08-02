e808db721254cae345c5930b8ef7b0c2c8a44a5d
Tarsis Lima <br.tmvdl@gmail.com>
2025-08-02 08:50:56 +0000

    Update endpoints list file

--- projects/binance-websocket-api/lists/endpoints.list.js
+++ projects/binance-websocket-api/lists/endpoints.list.js
@@ -1,9 +1,10 @@
 import { SocketEndpointModel } from '../../../assets/js/models/socket.endpoint.model.js'
-import * as BINANCE from '../../../assets/js/apis/binance.js'
+import * as BINANCE from '../../../assets/js/apis/rest/binance/v3.js'
 
 export const getEndpointsList = () => Array.from([
   new SocketEndpointModel('input', BINANCE.websocket.name),
   new SocketEndpointModel('input', 'klines', ['symbol', 'interval', 'startTime', 'limit']),
+  new SocketEndpointModel('input', 'SUBSCRIBE', ['symbol'])
 ])
 
 export const getInputEndpointsList = () => Array.from(getEndpointsList().filter(({ side }) => side == 'input'))
