e99619cc72fb36d90e129203ddcd8f2e73873da8
Tarsis Lima <br.tmvdl@gmail.com>
2026-01-03 11:37:43 -0300

    Add RealTime Webcam subproject's files

---
+++ projects/realtime-webcam/index.js
@@ -0,0 +1,29 @@
+const onOpenCvReady = () => {
+  const video = document.getElementById('videoInput')
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then((stream) => {
+      video.srcObject = stream
+      video.play()
+    })
+
+  const src = new cv.Mat(video.height, video.width, cv.CV_8UC4)
+  const gray = new cv.Mat(video.height, video.width, cv.CV_8UC1)
+  const dst = new cv.Mat(video.height, video.width, cv.CV_8UC1)
+  const cap = new cv.VideoCapture(video)
+
+  const processVideo = () => {
+    try {
+      cap.read(src)
+      cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY)
+      cv.Canny(gray, dst, 50, 150, 3, false)
+      cv.imshow('canvasOutput', dst)
+    } catch (err) {
+      console.error(err)
+    }
+    requestAnimationFrame(processVideo)
+  }
+
+  requestAnimationFrame(processVideo)
+}
+
+document.getElementById('buttonPlay').addEventListener('click', onOpenCvReady)
