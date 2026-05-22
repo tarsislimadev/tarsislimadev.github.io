70591b02f00b232e20984db9515a15efe5229ace
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-05-22 18:43:04 +0000

    feat: initialize product title elements and update DOM on load

--- assets/product.js
+++ assets/product.js
@@ -1,1 +1,9 @@
+const post_title_elements = document.getElementsByClassName('post_title');
+
 alert('product')
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM loaded');
+  const id = window.location.search.split('id=')[1];
+  post_title_elements.forEach(element => element.textContent = `Product ${id}`);
+});
