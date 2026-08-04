aafd55b85e251ec6d38f92eeb5fc2f9d20a27151
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-08-04 14:51:48 +0000

    feat: implement PeerJS integration for real-time peer ID display

--- projects/pixel/room.js
+++ projects/pixel/room.js
@@ -1,7 +1,22 @@
 import { HTML } from '../libs/afrontend/index.js'
+import { Peer } from "../libs/peerjs/index.js";
 
 export class Page extends HTML {
+  peer = new Peer()
+
+  el = {
+    peerIdDisplay: new HTML(),
+  }
+
+  constructor() {
+    super()
+    this.peer.on('open', (id) => {
+      this.el.peerIdDisplay.setText('My peer ID is: ' + id)
+    })
+  }
+
   onCreate() {
     this.setText('Pixel Room')
+    this.append(this.el.peerIdDisplay)
   }
 }
