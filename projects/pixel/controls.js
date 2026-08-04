888bfaf6ec81e4df4afcabc700984b5f0c3876ca
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-08-04 15:11:29 +0000

    feat: implement peer connection and display peer IDs in pixel room

--- projects/pixel/controls.js
+++ projects/pixel/controls.js
@@ -1,7 +1,50 @@
-import { HTML } from '../libs/afrontend/index.js'
+import { HTML, nLink } from '../libs/afrontend/index.js'
+import { Peer } from "../libs/peerjs/index.js";
 
 export class Page extends HTML {
+  peer = new Peer()
+
+  state = {
+    peerId: this.getPeerIdFromUrl(),
+    conn: null,
+  }
+
+  el = {
+    peerIdDisplay: new HTML(),
+  }
+
+  constructor() {
+    super()
+    this.state.conn = this.peer.connect(this.state.peerId)
+    this.setEvents()
+  }
+
+  setEvents() {
+    this.peer.on('open', (id) => {
+      const text = new HTML()
+      text.setText('My peer ID is: ' + id)
+      this.el.peerIdDisplay.append(text)
+    })
+    this.state.conn?.on('open', (id) => {
+      const roomId = new HTML()
+      roomId.setText('Room peer ID is: ' + id)
+      this.el.peerIdDisplay.append(roomId)
+    })
+    this.state.conn?.on('data', (data) => {
+      console.log('Received data:', data)
+    })
+    this.state.conn?.on('error', (err) => {
+      console.error('Connection error:', err)
+    })
+  }
+
   onCreate() {
-    this.setText('Pixel Controls')
+    this.setText('Pixel Room')
+    this.append(this.el.peerIdDisplay)
+  }
+
+  getPeerIdFromUrl() {
+    const urlParams = new URLSearchParams(window.location.search)
+    return urlParams.get('roomId') || undefined
   }
 }
